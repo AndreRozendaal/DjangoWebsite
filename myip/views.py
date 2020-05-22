1f071b7c63997ae4e8a69a9a290844a5f7481937
Andre Rozendaal <andrerozendaal@localhost.localdomain>
2020-05-22 10:44:45 +0200

    playing with myip

--- myip/views.py
+++ myip/views.py
@@ -4,6 +4,7 @@ def myip(request):
     context = { 
                     'myip' : request.META.get("REMOTE_ADDR"),
                     'myhost' : request.META.get("REMOTE_HOST"),
+                    'forwarded_for' : request.META.get("X_FORWARDED_FOR"),
               }
 
     return render(request, 'myip.html', context)
