5689493bcdb6dcf014ea2675c5aaeaa10c0e7ec6
Andre Rozendaal <andre@bitjes.com>
2020-06-30 16:04:04 +0200

    make seperate static folder

--- bitjes/settings.py
+++ bitjes/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESIRE = 'Strict'
 SESSION_COOKIE_SECURE = True
